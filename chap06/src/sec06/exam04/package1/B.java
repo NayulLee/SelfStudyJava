b735436a56c73c914b6e42d261cd1fd793b23a6d
Nayul <ahrvhrmsghk@gmail.com>
2025-02-04 15:56:13 +0900

    p.298 접근제한자 예제

---
+++ chap06/src/sec06/exam04/package1/B.java
@@ -0,0 +1,7 @@
+package sec06.exam04.package1;
+
+public class B {
+	A a1 = new A(true); // public 생성자 - 전부 가능
+	A a2 = new A(1);	// default 생성자 - 같은 패키지 내에서 가능
+	A a3 = new A("문자열");	// private 생성자 - 같은 클래스 내에서 가능(컴파일 오류)
+}
---
+++ chap06/src/sec06/exam04/package2/C.java
@@ -0,0 +1,9 @@
+package sec06.exam04.package2;
+
+import sec06.exam04.package1.A; // import문 써도 접근제한자 걸리면 못쓰는거다
+
+public class C {
+	A a1 = new A(true);		// public이라 다 가능
+	A a2 = new A(1);		// default 생성자 - 같은 패키지 내에서만 가능
+	A a3 = new A("문자열");	// private 생성자 - 같은 클래스 내에서만 가능
+}
