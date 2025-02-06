3c34525b39415a3f46985a1c2f14fb77dec6868e
Nayul <ahrvhrmsghk@gmail.com>
2025-02-06 13:48:15 +0900

    접근제한자 예제

--- chap06/src/sec06/exam05/package1/C.java
+++ chap06/src/sec06/exam05/package1/C.java
@@ -2,10 +2,18 @@ package sec06.exam05.package1;
 
 import sec06.exam05.package1.A;
 public class C {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		A a = new A();
 		
+		a.field1 = 1;
+		a.field2 = 1;
+		a.field3 = 1;
+		
+		a.method1();
+		a.method2();
+		a.method3();
 	}
 
 }
