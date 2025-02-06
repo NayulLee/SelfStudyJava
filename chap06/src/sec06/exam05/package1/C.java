60785488a981bf0e6df7b8da3030308375e52f3c
Nayul <ahrvhrmsghk@gmail.com>
2025-02-06 14:44:37 +0900

    컴파일 오류 예시문 주석처리로 수정

--- chap06/src/sec06/exam05/package1/C.java
+++ chap06/src/sec06/exam05/package1/C.java
@@ -9,11 +9,11 @@ public class C {
 		
 		a.field1 = 1;
 		a.field2 = 1;
-		a.field3 = 1;
+		// a.field3 = 1;
 		
 		a.method1();
-		a.method2();
-		a.method3();
+		// a.method2();
+		// a.method3();
 	}
 
 }
