a5d5326ee4cecfe6e44164f618461f59f2369f1a
Nayul <ahrvhrmsghk@gmail.com>
2025-02-13 14:34:07 +0900

    p.377 인터페이스 변수에 구현 객체 대입

---
+++ chap08/src/sec01/exam04/RemoteControlExample.java
@@ -0,0 +1,13 @@
+package sec01.exam04;
+import sec01.exam03.*;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+
+}
