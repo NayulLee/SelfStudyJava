f84b7c0601567b5ddbd9e2ba6570f48838a15af5
Nayul <ahrvhrmsghk@gmail.com>
2025-02-20 13:56:46 +0900

    p.417 중첩인터페이스(Button 클래스 구현 예제)

---
+++ chap09/src/sec01/exam06/MessageListener.java
@@ -0,0 +1,13 @@
+package sec01.exam06;
+
+import sec01.exam06.Button.OnClickListener;
+
+public class MessageListener implements OnClickListener {
+
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		System.out.println("메세지를 보냅니다.");
+	}
+
+}
