62ec7aa6d5ff5fc63f5e2d23114a5b29edbca835
Nayul <ahrvhrmsghk@gmail.com>
2025-02-20 14:30:46 +0900

    p.423 중첩인터페이스 확인문제 5

---
+++ chap09/src/sec01/verify/exam05/BackgroundChangeListener.java
@@ -0,0 +1,13 @@
+package sec01.verify.exam05;
+
+import sec01.verify.exam05.CheckBox.OnSelectListener;
+
+public class BackgroundChangeListener implements OnSelectListener {
+
+	@Override
+	public void onSelect() {
+		// TODO Auto-generated method stub
+		System.out.println("배경을 변경합니다.");
+	}
+
+}
