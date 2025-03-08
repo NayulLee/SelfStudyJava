b8408e1f4ffb6ea99b50be2ab578248885c2905b
Nayul <ahrvhrmsghk@gmail.com>
2025-03-08 18:26:00 +0900

    int or Integer 자료형 구분하는 메서드 추가

--- chap11/src/sec01/verify/exam08/StringConvertExample.java
+++ chap11/src/sec01/verify/exam08/StringConvertExample.java
@@ -1,11 +1,21 @@
 package sec01.verify.exam08;
 
+
 public class StringConvertExample {
 
+	public static void printType(int value) {
+		System.out.println(value + "는 int 타입입니다.");
+	}
+	
+	public static void printType(Integer value) {
+		System.out.println(value + "는 Integer 타입입니다.");
+	}
+	
 	public static void main(String[] args) {
 		String strData1 = "200";
 		int intData1 = Integer.parseInt(strData1);
 		System.out.println(((Integer)intData1).getClass().getName());
+		printType(intData1);
 		
 		int intData2 = 150;
 		String strData2 = String.valueOf(intData2);
